58d78f41648b3f3327874b8dad98ee1c9ee92b6a
jridder <jridder.neusta.de>
2025-01-24 18:52:37 +0100

    refactor: clean up formatting in WeatherDataFactoryTest for readability

--- WeatherForecastTest/Services/Factory/WeatherDataFactoryTest.cs
+++ WeatherForecastTest/Services/Factory/WeatherDataFactoryTest.cs
@@ -10,10 +10,9 @@ public class WeatherDataFactoryTest {
     public void Create_ReturnsValidWeatherData() {
         var jsonElement = JsonDocument.Parse(MockDataProvider.GetApiMockDataElement()).RootElement;
         var weatherData = WeatherDataFactory.Create(jsonElement);
-        
+
         Assert.That(weatherData, Is.InstanceOf<WeatherData>());
-        Assert.Multiple(() =>
-        {
+        Assert.Multiple(() => {
             Assert.That(weatherData.Hours, Is.EqualTo(1));
             Assert.That(weatherData.Temperature, Is.EqualTo(20));
             Assert.That(weatherData.Weather, Is.EqualTo("clear"));
